1d812423126678d0d5e4bc0d2cd2ac6be06330e4
wpe19d <WesEaston>
2021-06-02 12:57:17 -0500

    beautified the gauge

--- firegaugealert/src/App.vue
+++ firegaugealert/src/App.vue
@@ -1,13 +1,8 @@
 <template>
   <div id="app">
-    <h3>Will not stop at 100</h3>
-    <v-gauge :value="x" top :width="width" />
-    <hr />
-    <h3>Will stop at 100</h3>
-    <v-gauge :value="y" unit="%" :width="width" />
-    <hr />
-    <h3>Donut</h3>
-    <v-gauge :value="z" :options="options" :donut="true" />
+    <h1>Fire Level</h1>
+    <h3>{{fireLevels[fireLevelIndex]}}</h3>
+    <v-gauge :value="gaugeValue" :width="width" :height="height" :options="options" :animationSpeed="animationSpeed"/>
   </div>
 </template>
 
@@ -21,46 +16,65 @@ import VGauge from "vgauge";
   },
 })
 export default class App extends Vue {
-  x: number = 0;
-  y: number = 0;
-  z: number = 0;
+  
+  fireLevels: string[];
+  maxValue: number;
+  gaugeValue: number;
+  fireLevelIndex: number;
 
   data() {
+    this.fireLevels = ["Green", "Candle", "Woodwick", "Trashcan", "Dumpster", "Nuclear", "Apocalypse", "You're all fired"];
+    this.maxValue = 99
+    this.gaugeValue = 0;
+
+    this.fireLevelIndex = this.getFireLevelIndex(this.fireLevels, this.gaugeValue);
+
+    console.log(this.fireLevelIndex);
+
     return {
-      x: 0,
-      y: 0,
-      z: 0,
-      width: "400px",
+      width: "925px",
+      height: "725px",
+      animationSpeed: 100,
+      maxValue: this.maxValue,
+      gaugeValue: this.gaugeValue,
       options: {
-        angle: 1,
-        lineWidth: 0.1,
-        radiusScale: 1,
+        lines: 8,
+        angle: -0.1, // The span of the gauge arc
+        lineWidth: 0.15, // The line thickness
+        radiusScale: 1, // Relative radius
         pointer: {
-          length: 0.6,
-          strokeWidth: 0.035,
-          color: "#000000",
-        },
-        limitMax: false,
-        limitMin: false,
-        colorStart: "#6FADCF",
-        colorStop: "#8FC0DA",
-        strokeColor: "#E0E0E0",
+          length: 0.5, // // Relative to gauge radius
+          strokeWidth: 0.015, // The thickness
+          color: '#000000',
+      },
+        limitMax: true,
+        limitMin: true,
         generateGradient: true,
         highDpiSupport: true,
+        staticZones: [
+          {strokeStyle: "#009933", min: 0, max: 13},
+          {strokeStyle: "#1aff1a", min: 13, max: 25}, 
+          {strokeStyle: "#ccff66", min: 25, max: 38},
+          {strokeStyle: "#ffff00", min: 38, max: 50}, 
+          {strokeStyle: "#ffcc00", min: 50, max: 63}, 
+          {strokeStyle: "#ff9900", min: 63, max: 75}, 
+          {strokeStyle: "#ff6633", min: 75, max: 88}, 
+          {strokeStyle: "#ff0000", min: 88, max: 100}, 
+        ]
       },
     };
   }
-  mounted() {
-    setInterval(() => {
-      this.x += 4;
-    }, 1000);
-    setInterval(() => {
-      if (this.y < 100) this.y += 5;
-    }, 1000);
-    setInterval(() => {
-      if (this.z < 100 && this.z + 3 <= 100) this.z += 3;
-      else this.z += 100 - this.z;
-    }, 1000);
+
+  getFireLevelIndex(fireLevels: string[], gaugeValue: number) {
+
+    if(gaugeValue > 99) {
+      gaugeValue = 99;
+    }
+
+    let arraySize = fireLevels.length;
+
+    let index = Math.floor(((gaugeValue / 100 ) * arraySize));
+    return index;
   }
 }
 </script>
@@ -73,5 +87,8 @@ export default class App extends Vue {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.gauge-title {
+  visibility: hidden;
 }
 </style>
