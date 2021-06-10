bd31662a4077796213b3830050eab7cbcb8c68ad
Wes Easton <wpe19d@gmail.com>
2021-06-10 14:46:45 -0500

    Update App.vue
    update fire level

--- firegaugealert/src/App.vue
+++ firegaugealert/src/App.vue
@@ -37,7 +37,7 @@ export default class App extends Vue {
     Apocalypse: 75 - 87
     You're all fired 88 - 99
     */
-    this.gaugeValue = 30;
+    this.gaugeValue = 47;
 
     this.fireLevelIndex = this.getFireLevelIndex(this.fireLevels, this.gaugeValue);
 
